599150be44e92bf4bb675cb3cf3840b29c46d128
JAY Adhav <101917934+jayadhav07@users.noreply.github.com>
2022-10-30 12:03:06 +0530

    Add files via upload

---
+++ src/Footer.jsx
@@ -0,0 +1,43 @@
+import { Heading, Stack, VStack ,Box, HStack, Button, Input,Text} from '@chakra-ui/react'
+import React from 'react'
+import {AiOutlineSend} from 'react-icons/ai'
+
+function Footer() {
+  return (
+    <Box bgColor = {"blackAlpha.900"} miniH={"40"} p ="16" color={"white"}>
+      <Stack direction={['column','row']}>
+        <VStack alignItems={'stretch'} w={'full'} px={'4'} >
+          <Heading size='md' textTransform={'uppercase'} textAlign={'center'}>
+         Subscribe to get Updates
+          </Heading>
+          <HStack borderBottom={'2px solid white'} py='2'>
+            <Input placeholder='Enter Email Here..' border={'none'} borderRadius={'none'} outline={'none'} focusBorderColor={'none'}/>
+            <Button p={'0'} colorScheme={"purple"} variant={"ghost"} borderRadius={"0ps 20px 20px 0"}>
+              <AiOutlineSend size={20}/>
+            </Button>
+          </HStack>
+
+        </VStack>
+
+        <VStack w={'full'} borderLeft={['none','1px solid white']}
+        borderRight={['none','1px solid white']}
+        >
+          <Heading textTransform={"uppercase"} textAlign = {"center"} >VIDEO HUB</Heading>
+          <Text>All Rights Recieved</Text>
+        </VStack>
+        <VStack w={'full'}>
+          <Heading size={'md'} textTransform={'uppercase'} >Social Media</Heading>
+          <Button variant={'link'}colorScheme={'white'} ><a target={'_blank'}  href='https://www.youtube.com'>Youtube</a></Button>
+
+          <Button variant={'link'}colorScheme={'white'} ><a target={'_blank'}  href='https://www.instagram.com'>Instagram</a></Button>
+
+          <Button variant={'link'}colorScheme={'white'} ><a target={'_blank'} href='https://www.github.com'>Github</a></Button>
+
+        </VStack>
+      </Stack>
+
+    </Box>
+  )
+}
+
+export default Footer
